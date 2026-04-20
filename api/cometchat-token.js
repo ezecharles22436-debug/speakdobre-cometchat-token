3152a5d8c464f98caee70366301e06e8045e9cf4
ezecharles22436-debug <ezecharles22436@gmail.com>
2026-04-20 09:45:24 +0300

    Update cometchat-token.js

--- api/cometchat-token.js
+++ api/cometchat-token.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Allow CORS (so Webflow can call it)
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
