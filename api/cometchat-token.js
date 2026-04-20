73461df3db2e48adb62946275c18f610b2a7cbe5
ezecharles22436-debug <ezecharles22436@gmail.com>
2026-04-20 09:49:25 +0300

    Update cometchat-token.js

--- api/cometchat-token.js
+++ api/cometchat-token.js
@@ -1,5 +1,4 @@
 module.exports = async function handler(req, res) {
-  // Allow CORS (so Webflow can call it)
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -13,7 +12,7 @@ module.exports = async function handler(req, res) {
   }
 
   try {
-    const { uid, name } = req.body;
+    const { uid, name } = req.body || {};
 
     if (!uid) {
       return res.status(400).json({ error: "Missing uid" });
@@ -23,12 +22,15 @@ module.exports = async function handler(req, res) {
     const REGION = "eu";
     const API_KEY = process.env.COMETCHAT_API_KEY;
 
-    // 1. Create user (safe if already exists)
-    await fetch(https://${APP_ID}.api-${REGION}.cometchat.io/v3/users, {
+    if (!API_KEY) {
+      return res.status(500).json({ error: "Missing COMETCHAT_API_KEY" });
+    }
+
+    await fetch(`https://${APP_ID}.api-${REGION}.cometchat.io/v3/users`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "apikey": API_KEY
+        apikey: API_KEY
       },
       body: JSON.stringify({
         uid: uid,
@@ -36,13 +38,12 @@ module.exports = async function handler(req, res) {
       })
     });
 
-    // 2. Generate auth token
     const tokenRes = await fetch(
-      https://${APP_ID}.api-${REGION}.cometchat.io/v3/users/${uid}/auth_tokens,
+      `https://${APP_ID}.api-${REGION}.cometchat.io/v3/users/${uid}/auth_tokens`,
       {
         method: "POST",
         headers: {
-          "apikey": API_KEY
+          apikey: API_KEY
         }
       }
     );
@@ -50,11 +51,13 @@ module.exports = async function handler(req, res) {
     const tokenData = await tokenRes.json();
 
     return res.status(200).json({
-      token: tokenData.data.authToken
+      token: tokenData?.data?.authToken || null,
+      raw: tokenData
     });
-
   } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: "Server error" });
+    return res.status(500).json({
+      error: "Server error",
+      details: err.message
+    });
   }
-}
+};
